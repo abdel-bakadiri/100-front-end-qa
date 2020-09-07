9d73c7c99b3f9fa99b12e068c3418d45807320d3
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-09-07 16:41:46 +0200

    feat(anonymous function): Anonymous function in JS

---
+++ anonymous-function.js
@@ -0,0 +1,8 @@
+// normal function
+function add(num1, num2) {
+  return num1 + num2;
+}
+console.log(add(2, 2));
+// anonymous function
+const anf = (param1, param2) => param1 + param2;
+console.log(anf(10, 20));

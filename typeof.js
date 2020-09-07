5adb03dbc692a50837218f38d73345fc2c08cc2c
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-09-07 14:45:49 +0200

    feat(typeof): All posibilities to use typeof in JS

---
+++ typeof.js
@@ -0,0 +1,13 @@
+console.log(typeof 73); // number
+console.log(typeof `literal template`); // string
+console.log(typeof NaN); // number
+console.log(typeof []); // object
+console.log(typeof {}); // object
+console.log(typeof function () {}); //function
+console.log(typeof null); //object
+console.log(null instanceof Object); //false
+console.log({} instanceof Object); // true
+console.log([] instanceof Object); // true
+console.log(function () {} instanceof Object);
+console.log(Array.isArray([])); // true
+console.log(Array.isArray({})); // true

9efe7fb00a74a36b875ccdf53d6b3299f971927f
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-09-08 21:38:48 +0200

    Feat(): IIFE

---
+++ iife.js
@@ -0,0 +1,12 @@
+const res = (function () {
+  const init = {
+    id: 0,
+    methods: {
+      someThing: 0,
+      test: function call() {},
+    },
+  };
+  return init;
+})();
+
+console.log(res); //

ae416f6817d1441ec7ebd005e598d7060516b1e4
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-09-10 22:04:50 +0200

    Feat(): destroctoring objects and arrays in JS

---
+++ destructoring-arrayobject.js
@@ -0,0 +1,9 @@
+const [day, mount, year] = ["10", "09", "2020"];
+console.log(`${day}/${mount}/${year}`); // 10/09/2020
+let { firstName: fn, cuntry, city, score } = {
+  firstName: "Abdel",
+  cuntry: "France",
+  city: "Paris",
+  score: 100,
+};
+console.log(`${fn} lives in ${city}`);

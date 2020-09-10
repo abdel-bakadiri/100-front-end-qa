aaabba15231fe29c5e96a46ace167c31881eedc8
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-09-10 22:57:45 +0200

    Feat(): NaN, type of NaN, isNaN ...

---
+++ nan.js
@@ -0,0 +1,6 @@
+console.log(0 / 0); // NaN
+console.log(Number("g")); // Nan
+console.log(typeof NaN); // NaN is consider as a number in JS !!!
+console.log(isNaN(76)); //False
+console.log(isNaN(NaN)); //true
+
